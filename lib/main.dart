513dad1656af005d76a08aa3660c66c7528fbbd6
sifrebilmiyorum27-wq <sifrebilmiyorum27@gmail.com>
2025-11-07 10:53:50 +0300

    Add main function and MyApp widget for Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,21 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
