7e42b041e652b672770efbe489f9291fdfb411ef
sifrebilmiyorum27-wq <sifrebilmiyorum27@gmail.com>
2025-11-07 10:54:21 +0300

    Add initial list of topics in konular.dart

---
+++ lib/data/konular.dart
@@ -0,0 +1,6 @@
+List<Map<String, dynamic>> konular = [
+  {"ad": "Temel Kavramlar", "tamamlandi": false},
+  {"ad": "Rasyonel Sayılar", "tamamlandi": false},
+  {"ad": "Problemler", "tamamlandi": false},
+  {"ad": "Fonksiyonlar", "tamamlandi": false},
+];
